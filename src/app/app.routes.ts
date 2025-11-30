81750ace10e4f14003d4dff0384c10eb2f1929fa
MadalinaBuu <madalina.ciucioiu@gmail.com>
2025-11-30 13:09:09 +0200

    projects in navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },       // Home page route
-  { path: 'about', component: AboutComponent }  // About page route
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: CarouselComponent }
 ];
