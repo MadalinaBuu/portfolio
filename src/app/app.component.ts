306b1c30020abfc480cc314d1aebc95b9f7d31da
MadalinaBuu <madalina.ciucioiu@gmail.com>
2025-11-30 13:05:12 +0200

    added navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CarouselComponent } from './carousel/carousel.component';
 import { AboutComponent } from "./about/about.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
